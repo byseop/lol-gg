0887026cfe7a586f6d3ab775922beffd8c55e7ef
Youngseop Bae <byseop@gmail.com>
2020-07-01 14:21:03 +0900

    ADD: styled component resets

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import MainRoutes from './pages/MainRoutes';
+import { Reset } from 'styled-reset';
 
 function App() {
   return (
     <div className="App">
+      <Reset />
       <MainRoutes />
     </div>
   );
