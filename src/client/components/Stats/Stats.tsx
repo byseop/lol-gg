b9530f7bf199d93876de654b08852173bffcbe88
Youngseop Bae <byseop@gmail.com>
2020-07-04 00:58:39 +0900

    ADD: header

--- src/client/components/Stats/Stats.tsx
+++ src/client/components/Stats/Stats.tsx
@@ -2,20 +2,24 @@ import React from 'react';
 import ProfileContainer from './Profile';
 import { SummonerInfoTypes } from './types';
 import Inner from '../Layout/Inner';
+import Header from '../Layout/Header';
 
 type StatsPropTypes = {
   summonerInfo: SummonerInfoTypes | undefined;
 };
 
 export default function Stats({ summonerInfo }: StatsPropTypes) {
-  console.log(summonerInfo);
-  if (summonerInfo) {
-    const { name, profileIconId } = summonerInfo;
-    return (
-      <Inner>
-        <ProfileContainer name={name} profileIconId={profileIconId} />
-      </Inner>
-    );
-  }
-  return null;
+  return (
+    <>
+      <Header />
+      {summonerInfo && summonerInfo.name && summonerInfo.profileIconId && (
+        <Inner>
+          <ProfileContainer
+            name={summonerInfo.name}
+            profileIconId={summonerInfo.profileIconId}
+          />
+        </Inner>
+      )}
+    </>
+  );
 }
